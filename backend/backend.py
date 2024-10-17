65b32b7542ad1281a9e6088b0d7162c849ed657d
Flysqu <98214547+flysqu@users.noreply.github.com>
2024-10-18 00:07:34 +0200

    Update backend.py

--- backend/backend.py
+++ backend/backend.py
@@ -10,10 +10,7 @@ app = FastAPI()
 
 # Define allowed origins (frontend URLs that should be allowed to call the API)
 origins = [
-    "http://localhost:8001",
-    "http://127.0.0.1:8001",
-    "http://127.0.0.1:8001/home.html?",
-    "https://anarchy-chat-lern.onrender.com/"
+    "*"
 ]
 
 # Add CORS middleware to the FastAPI app
